8ba648dcaecfe4848d259e77ec8a9193b80c96ca
Xiulan Shi <xiulanshi2013@gmail.com>
2016-03-01 21:20:05 -0500

    Updated

---
+++ Rosary/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Rosary
+//
+//  Created by Xiulan Shi on 3/1/16.
+//  Copyright © 2016 Xiulan Shi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Rosary/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Rosary
+//
+//  Created by Xiulan Shi on 3/1/16.
+//  Copyright © 2016 Xiulan Shi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
